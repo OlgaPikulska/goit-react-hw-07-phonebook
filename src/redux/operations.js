9d4c25a3fb25d658ef20405c77e517e8d4d115d6
Olga Pikulska <olga.pikulska.ld@gmail.com>
2023-03-17 09:06:43 +0100

    Install axios, create store and basic operations

---
+++ src/redux/operations.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+import { createAsyncThunk } from "@reduxjs/toolkit";
+
+axios.defaults.baseURL = "https://64140e9eebce1f9d8c5df603.mockapi.io";
+
+export const fetchContacts = createAsyncThunk("contacts/fetchAll", async (_, thunkAPI) => {
+    try {
+        const response = await axios.get("/contacts");
+        console.log(response);
+        return response
+    } catch (e) {
+        return thunkAPI.rejectWithValue(e.message)
+    }
+});
+
+export const addContact = createAsyncThunk("contacts/addContact", async (contact, thunkAPI) => {
+    try {
+        const response = await axios.post("/contacts", { name, number, id });
+        console.log(response)
+        return response
+    } catch (e) {
+        return thunkAPI.rejectWithValue(e.message);
+    }
+})
